7aa2669237ad4930b8c4bd43afdf2d90a0d5a406
David Chester <david@fmail.co.uk>
2025-05-20 23:13:43 -0400

    add example

---
+++ example/store.js
@@ -0,0 +1,44 @@
+import { store } from './peak.js'
+
+store.initialize = () => {
+  Object.assign(store, {
+    filter: 'all',
+    items: [
+      { id: id(), done: false, title: 'Walk the dog' },
+      { id: id(), done: false, title: 'Do the dishes' },
+      { id: id(), done: false, title: 'Mow the lawn' },
+    ],
+    setFilter(v) {
+      this.filter = v
+    },
+    setItem(item, data) {
+      const idx = this.items.findIndex(i => i == item)
+      Object.assign(this.items[idx], data)
+    },
+    removeItem(i) {
+      this.items.splice(i, 1)
+    },
+    getFilteredItems() {
+      return this.items.filter(i => {
+        if (this.filter == 'all') return true
+        if (this.filter == 'completed' && i.done) return true
+        if (this.filter == 'active' && !i.done) return true
+      })
+    },
+    clearCompleted() {
+      this.items = this.items.filter(i => !i.done)
+    },
+    addItem(title) {
+      store.items.unshift({ id: id(), title, done: false })
+    }
+  })
+}
+
+window.store = store
+
+function id() {
+  return Math.random().toString(36).slice(2)
+}
+
+export { store }
+
