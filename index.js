cec510218edd141f9dc8bdffbf45f4d373f8f0c4
David Chester <david@fmail.co.uk>
2025-05-23 22:59:57 -0400

    wip el => peak

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-import Alpine from './alpine.js'
 const parser = new DOMParser
-window.Alpine = Alpine
-Alpine.start()
-
-Alpine.store('global', {})
-export const store = Alpine.store('global')
+const contexts = new WeakMap
+const handlers = {}
 
 export const route = {}
 
@@ -34,25 +30,32 @@ export const component = async (tagName, path) => {
 
   const src = await fetch(path).then(r => r.text())
   const doc = parser.parseFromString(src, 'text/html')
-  const template = doc.querySelector('template')?.innerHTML
+  const _template = doc.querySelector('template')
+  const template = document.createElement('div')
+  template.innerHTML = `<div>${_template.innerHTML}</div>`
   const style = doc.querySelector('style')?.textContent
   const script = doc.querySelector('script')?.textContent 
   const _class = await loadModule(script, path)
 
-  const key = tagName.replace(/\W/g, '_')
-
-  Alpine.data(key, () => {
-    const instance = new (_class || new Function)
-    return instance
-  })
+  const instance = new (_class || new Function)
 
   class _constructor extends HTMLElement {
     constructor() {
       super()
-      this.innerHTML = template
-      this.setAttribute('x-data', key)
+      this.created?.()
+    }
+    connectedCallback() {
+      this.render()
+      this.mounted?.()
+    }
+    render() {
+      morph(this, render(template, this))
     }
   }
+
+  Object.getOwnPropertyNames(_class.prototype)
+    .filter(n => n != 'constructor')
+    .forEach(n => _constructor.prototype[n] = _class.prototype[n]);
 
   customElements.define(tagName, _constructor)
 
@@ -147,7 +150,6 @@ async function loadModule(source='', url) {
       injectedVars.push(specifiers.trim());
       injectedValues.push(mod.default);
     } else {
-      // Named imports
       const names = specifiers
         .replace(/[{}]/g, '')
         .split(',')
@@ -174,4 +176,134 @@ async function loadModule(source='', url) {
 
   const factory = new Function(...injectedVars, rewrittenSource);
   return factory(...injectedValues);
+}
+
+function render(template, ctx) {
+  const root = template.cloneNode(true)
+  function _render(el) {
+    if (!el.children.length && el.hasAttribute('x-text')) {
+      el.textContent = evalInContext(ctx, el.getAttribute('x-text'))
+      el.removeAttribute('x-text')
+    }
+    if (!el.children.length && el.hasAttribute('x-html')) {
+      el.innerHTML = evalInContext(ctx, el.getAttribute('x-html'))
+      el.removeAttribute('x-html')
+    }
+    for (const a of el.attributes || []) {
+      if (a.name.startsWith(':')) {
+        el.setAttribute(a.name.slice(1), evalInContext(ctx, a.value))
+        el.removeAttribute(a.name)
+      }
+      if (a.name.startsWith('@')) {
+        const eventName = a.name.slice(1)
+        listen(eventName, el, ctx)
+      }
+    }
+    for (const c of el.children) _render(c)
+    return el
+  }
+  return _render(root)
+}
+
+function listen(eventType, el, ctx) {
+  contexts.set(el, ctx)
+  if (handlers[eventType]) return
+  const handler = (e) => handleEvent(e, eventType)
+  handlers[eventType] = true
+  document.addEventListener(eventType, handler, true)
+}
+
+function handleEvent(event, eventType) {
+  let target = event.target
+  
+  while (target && target !== document) {
+    const ctx = contexts.get(target)
+    let value = target.getAttribute(`@${eventType}`)
+    
+    if (ctx && value) {
+      try {
+        if (value in ctx) value += '()'
+        evalInContext(ctx, value, event)
+        if (event.defaultPrevented || event.cancelBubble) break
+      } catch (err) {
+        console.error(err)
+      }
+    }
+    target = target.parentElement
+  }
+}
+
+
+function morph(l, r) {
+  let ls = 0, rs = 0, le = l.childNodes.length, re = r.childNodes.length
+  const lc = [...l.childNodes], rc = [...r.childNodes]
+  const content = e => e.nodeType == 3 ? e.textContent : e.nodeType == 1 ? e.outerHTML : ''
+  const key = e => e.nodeType == 1 && customElements.get(e.tagName.toLowerCase()) && e.getAttribute('key') || NaN
+
+  for (const a of r.attributes || [])
+    if (l.getAttribute(a.name) != a.value) {
+      l.setAttribute(a.name, a.value)
+      if (l.constructor.prototype.hasOwnProperty(a.name) && typeof l[a.name] == 'boolean')
+        l[a.name] = true
+    }
+  for (const a of l.attributes || [])
+    if (!r.hasAttribute(a.name)) {
+      l.removeAttribute(a.name)
+      if (l.constructor.prototype.hasOwnProperty(a.name) && typeof l[a.name] == 'boolean')
+        l[a.name] = false
+    }
+
+  while (ls < le || rs < re)
+    if (ls == le) l.insertBefore(lc.find(l => key(l) == key(rc[rs])) || rc[rs], lc[ls]) && rs++
+    else if (rs == re) l.removeChild(lc[ls++])
+    else if (content(lc[ls]) == content(rc[rs])) ls++ & rs++
+    else if (content(lc[le - 1]) == content(rc[re - 1])) le-- & re--
+    else if (lc[ls] && rc[rs].children && lc[ls].tagName == rc[rs].tagName) morph(lc[ls++], rc[rs++])
+    else lc[ls++].replaceWith(rc[rs++].cloneNode(true))
+} 
+
+function evalInContext(element, code, ...args) {
+  const wrappedCode = code.replace(
+    /(?<![\w$.])\b([a-zA-Z_$][a-zA-Z0-9_$]*)\b(?=\s*(?:[^\w$\s]|$))/g,
+    (match, identifier) => {
+      const keywords = [ 'true', 'false', 'null', 'undefined', 'NaN', 'Infinity', 'Math', 'Date', 'String', 'Number', 'Object', 'Array', 'console', 'window', 'document', 'JSON', 'Promise', 'let', 'const', 'var', 'function', 'return', 'if', 'else', 'for', 'while', 'do', 'switch', 'case', 'break', 'continue', 'try', 'catch', 'finally', 'throw', 'new', 'delete', 'typeof', 'instanceof', 'in', 'of', 'class', 'extends', 'super', 'this' ]
+      if (keywords.includes(identifier)) return match
+      return `this.${identifier}`
+    }
+  );
+  
+  return new Function(`return ${wrappedCode}`).call(element, ...args);
+}
+
+let _contextId
+const deps = {}
+
+function notify(path) {
+  for (const id in deps[path] || {}) {
+    setTimeout(_ => deps[path][id]())
+  }
+}
+
+function dep(path) {
+  dep._path = !_contextId && path
+  if (!_contextId) return true
+  const contextId = _contextId
+  deps[path] ||= {}
+  return deps[path][_contextId] = () => el.render()
+}
+
+function observable(x, path = Math.random().toString(36).slice(2)) {
+  if ((typeof x != 'object' || x === null) && dep(path)) return x
+  return new Proxy(x, {
+    set(x, key) {
+      return notify(path + '/' + key) || Reflect.set(...arguments);
+    },
+    get(x, key) {
+      return x.__target__ ? x[key]
+        : typeof key == "symbol" ? Reflect.get(...arguments)
+        : (key in x.constructor.prototype && dep(path + '/' + key)) ? x[key]
+        : (key == '__target__') ? x
+        : observable(x[key], path + '/' + key);
+    }
+  });
 }
