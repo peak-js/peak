c554229b74b825be3f03451daf4c8829b1b35feb
David Chester <david@fmail.co.uk>
2025-06-02 22:37:51 -0400

    tweak playwright test config

--- tests/playwright.config.js
+++ tests/playwright.config.js
@@ -2,7 +2,7 @@ const { defineConfig, devices } = require('@playwright/test');
 
 module.exports = defineConfig({
   use: {
-    baseURL: process.env.BASE_URL || 'https://localhost:8000',
+    baseURL: process.env.BASE_URL || 'http://localhost:8000',
     headless: true,
   },
   projects: [
