d077abe8e7c79ca06056ca909443dcc0ca7b9dbd
David Chester <david@frameable.com>
2025-07-08 23:46:37 -0400

    update playwright test config

--- tests/playwright.config.js
+++ tests/playwright.config.js
@@ -1,9 +1,15 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test'
+
+const PORT = 3712
 
 export default defineConfig({
   use: {
-    baseURL: process.env.BASE_URL || 'http://localhost:8000',
+    baseURL: process.env.BASE_URL || `http://localhost:${PORT}`,
     headless: true,
+  },
+  webServer: {
+    command: `python -m http.server ${PORT} 2>/dev/null`,
+    port: PORT,
   },
   projects: [
     { name: 'firefox', ...devices['Desktop Firefox'] },
@@ -12,4 +18,4 @@ export default defineConfig({
   expect: {
     timeout: 5000
   },
-});
+})
